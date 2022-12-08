7dbad49764a3d1db84e9a4a0a4fc4c1c9ce338e7
Lizaveta <lizadondysh@icloud.com>
2022-12-09 00:59:09 +0300

    feat: add solutions for 05-objects-tasks

--- src/05-objects-tasks.js
+++ src/05-objects-tasks.js
@@ -20,8 +20,14 @@
  *    console.log(r.height);      // => 20
  *    console.log(r.getArea());   // => 200
  */
-function Rectangle(/* width, height */) {
-  throw new Error('Not implemented');
+function Rectangle(width, height) {
+  return {
+    width,
+    height,
+    getArea() {
+      return width * height;
+    },
+  };
 }
 
 
@@ -35,8 +41,8 @@ function Rectangle(/* width, height */) {
  *    [1,2,3]   =>  '[1,2,3]'
  *    { width: 10, height : 20 } => '{"height":10,"width":20}'
  */
-function getJSON(/* obj */) {
-  throw new Error('Not implemented');
+function getJSON(obj) {
+  return JSON.stringify(obj);
 }
 
 
@@ -51,8 +57,8 @@ function getJSON(/* obj */) {
  *    const r = fromJSON(Circle.prototype, '{"radius":10}');
  *
  */
-function fromJSON(/* proto, json */) {
-  throw new Error('Not implemented');
+function fromJSON(proto, json) {
+  return Object.setPrototypeOf(JSON.parse(json), proto);
 }
 
 
@@ -110,33 +116,179 @@ function fromJSON(/* proto, json */) {
  *  For more examples see unit tests.
  */
 
+class Builder {
+  constructor() {
+    this.selectorMap = {
+      tag: '',
+      id: '',
+      classes: [],
+      attributes: [],
+      pseudoClasses: [],
+      pseudoElement: '',
+    };
+
+    this.logger = {
+      tagCount: 0,
+      idCount: 0,
+      classesCount: 0,
+      attributesCount: 0,
+      pseudoClassesCount: 0,
+      pseudoElementCount: 0,
+    };
+  }
+
+  element(value) {
+    if (this.isApplicable('element')) {
+      this.selectorMap.tag = `${value}`;
+    }
+
+    return this;
+  }
+
+  id(value) {
+    if (this.isApplicable('id')) {
+      this.selectorMap.id = `#${value}`;
+    }
+
+    return this;
+  }
+
+  class(value) {
+    if (this.isApplicable('class')) {
+      this.selectorMap.classes.push(`.${value}`);
+    }
+
+    return this;
+  }
+
+  attr(value) {
+    if (this.isApplicable('attr')) {
+      this.selectorMap.attributes.push(`[${value}]`);
+    }
+
+    return this;
+  }
+
+  pseudoClass(value) {
+    if (this.isApplicable('pseudo-class')) {
+      this.selectorMap.pseudoClasses.push(`:${value}`);
+    }
+
+    return this;
+  }
+
+  pseudoElement(value) {
+    if (this.isApplicable('pseudo-element')) {
+      this.selectorMap.pseudoElement = `::${value}`;
+    }
+
+    return this;
+  }
+
+  stringify() {
+    return Object.values(this.selectorMap).flat().join('');
+  }
+
+  isApplicable(calledMethod) {
+    if (calledMethod === 'element') {
+      if (this.logger.tagCount) {
+        throw new Error('Element, id and pseudo-element should not occur more then one time inside the selector');
+      }
+      if (Object.values(this.logger).reduce((prev, curr) => prev + curr, 0)) {
+        throw new Error('Selector parts should be arranged in the following order: element, id, class, attribute, pseudo-class, pseudo-element');
+      }
+
+      this.logger.tagCount += 1;
+      return true;
+    }
+
+    if (calledMethod === 'id') {
+      if (this.logger.idCount) {
+        throw new Error('Element, id and pseudo-element should not occur more then one time inside the selector');
+      }
+      if ([...Object.values(this.logger).slice(1)].reduce((prev, curr) => prev + curr, 0)) {
+        throw new Error('Selector parts should be arranged in the following order: element, id, class, attribute, pseudo-class, pseudo-element');
+      }
+
+      this.logger.idCount += 1;
+      return true;
+    }
+
+    if (calledMethod === 'class') {
+      if ([...Object.values(this.logger).slice(3)].reduce((prev, curr) => prev + curr, 0)) {
+        throw new Error('Selector parts should be arranged in the following order: element, id, class, attribute, pseudo-class, pseudo-element');
+      }
+
+      this.logger.classesCount += 1;
+      return true;
+    }
+
+    if (calledMethod === 'attr') {
+      if ([...Object.values(this.logger).slice(4)].reduce((prev, curr) => prev + curr, 0)) {
+        throw new Error('Selector parts should be arranged in the following order: element, id, class, attribute, pseudo-class, pseudo-element');
+      }
+
+      this.logger.attributesCount += 1;
+      return true;
+    }
+
+    if (calledMethod === 'pseudo-class') {
+      if ([...Object.values(this.logger).slice(5)].reduce((prev, curr) => prev + curr, 0)) {
+        throw new Error('Selector parts should be arranged in the following order: element, id, class, attribute, pseudo-class, pseudo-element');
+      }
+
+      this.logger.pseudoClassesCount += 1;
+      return true;
+    }
+
+    if (calledMethod === 'pseudo-element') {
+      if (this.logger.pseudoElementCount) {
+        throw new Error('Element, id and pseudo-element should not occur more then one time inside the selector');
+      }
+
+      this.logger.pseudoElementCount += 1;
+      return true;
+    }
+
+    return null;
+  }
+}
+
 const cssSelectorBuilder = {
-  element(/* value */) {
-    throw new Error('Not implemented');
+  element(value) {
+    return new Builder().element(value);
   },
 
-  id(/* value */) {
-    throw new Error('Not implemented');
+  id(value) {
+    return new Builder().id(value);
   },
 
-  class(/* value */) {
-    throw new Error('Not implemented');
+  class(value) {
+    return new Builder().class(value);
   },
 
-  attr(/* value */) {
-    throw new Error('Not implemented');
+  attr(value) {
+    return new Builder().attr(value);
   },
 
-  pseudoClass(/* value */) {
-    throw new Error('Not implemented');
+  pseudoClass(value) {
+    return new Builder().pseudoClass(value);
   },
 
-  pseudoElement(/* value */) {
-    throw new Error('Not implemented');
+  pseudoElement(value) {
+    return new Builder().pseudoElement(value);
   },
 
-  combine(/* selector1, combinator, selector2 */) {
-    throw new Error('Not implemented');
+  combine(selector1, combinator, selector2) {
+    return {
+      selector1Str: selector1.stringify(),
+      selector2Str: selector2.stringify(),
+      combinator,
+
+      stringify() {
+        return `${this.selector1Str} ${this.combinator} ${this.selector2Str}`;
+      },
+    };
   },
 };
 
